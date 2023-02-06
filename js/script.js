4b3c61eb65a2a791f8ed8e08cad8b03c09ca0261
Agustin Schniepp <103939898+agusschniepp@users.noreply.github.com>
2023-02-05 21:29:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.js'
+import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
